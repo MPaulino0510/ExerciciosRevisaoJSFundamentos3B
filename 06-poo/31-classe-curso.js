e741129d2391bde8e9d46e50930a702c0f3fb590
MIGUEL PAULINO ALVES PEREIRA <miguelpaulino2009.ap@gmail.com>
2026-08-25 14:46:07 -0300

    Salvando

--- 06-poo/31-classe-curso.js
+++ 06-poo/31-classe-curso.js
@@ -5,3 +5,19 @@
 //
 // Escreva sua solução abaixo:
 
+class Curso{
+    constructor(nome, duracao, modalidade){
+        this.nome = nome;
+        this.duracao = duracao;
+        this.modalidade = modalidade;
+    }
+    exibirInformacoes(){
+        console.log(this.nome, this.duracao, this.modalidade);
+    }
+}
+
+const curso1 = new Curso("Senai", 50, "DS");
+const curso2 = new Curso("Etec", 20, "DS");
+
+curso1.exibirInformacoes();
+curso2.exibirInformacoes();
