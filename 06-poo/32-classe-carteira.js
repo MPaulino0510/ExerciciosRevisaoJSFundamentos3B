f0d649aa725c022b48e7bc5b303388f61a1bb055
MIGUEL PAULINO ALVES PEREIRA <miguelpaulino2009.ap@gmail.com>
2026-08-25 15:28:33 -0300

    Salvando

--- 06-poo/32-classe-carteira.js
+++ 06-poo/32-classe-carteira.js
@@ -5,3 +5,20 @@
 //
 // Escreva sua solução abaixo:
 
+class CarteiraDigital{
+    #saldo = 0;
+
+    adicionarCredito(valor){
+        if(valor > 0){this.#saldo += valor;}
+    }
+    realizarPagamento(valor){
+        if(this.#saldo >= valor && valor > 0) {this.#saldo -= valor;
+        return true;
+        }
+        return false;
+    }
+    consultarSaldo(){
+        console.log(this.#saldo);
+        return this.#saldo;
+    }
+}
--- 06-poo/33-get-set-aluno.js
+++ 06-poo/33-get-set-aluno.js
@@ -5,3 +5,28 @@
 //
 // Escreva sua solução abaixo:
 
+class Aluno{
+    #nota;
+
+    constructor(nota){
+        this.nota = nota;
+    }
+    get nota(){
+        return this.#nota;
+    }
+    set nota(valor){
+        if(valor > 0 && valor <= 10){
+            this.#nota = valor;
+            } else {
+                console.log("Nota inválida! Deve ser entre 0 e 10.");
+            };
+    }
+}
+
+const aluno1 = new Aluno(8);
+console.log("Nota inicial:", aluno1.nota);
+
+aluno1.nota = 9.5;
+console.log("Nota alterada:", aluno1.nota);
+
+aluno1.nota = 15;
